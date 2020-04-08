8bd3f6a62abe6ea6067ec5389e9c58ab6ea90893
TheXenonPhoenix <majorgamerkevin@gmail.com>
2020-04-08 10:14:08 -0500

    added pages: About, AuditSearch, and InventorySearch

---
+++ app/src/main/java/com/example/inventoryauditapp/AuditSearchPage.java
@@ -0,0 +1,14 @@
+package com.example.inventoryauditapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AuditSearchPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_audit_search_page);
+    }
+}
