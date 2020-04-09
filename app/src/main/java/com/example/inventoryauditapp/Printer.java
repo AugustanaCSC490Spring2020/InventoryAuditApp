66f0b63e3f439c7cb159ea186dd57e701801405a
Dylan Hart <dylanhart16@augustana.edu>
2020-04-08 19:39:55 -0500

    Added custom data type for Printer.java for user later on in the project.

---
+++ app/src/main/java/com/example/inventoryauditapp/Printer.java
@@ -0,0 +1,99 @@
+package com.example.inventoryauditapp;
+
+import java.util.Date;
+
+public class Printer {
+
+    private int idNumber;
+    private String building;
+    private int roomNumber;
+    private String type;
+    private String brand;
+    private Date dateAdded;
+    private User modifiedBy;
+
+    public Printer() {
+        //empty constructor required by Firebase
+    }
+
+    // constructor
+    public Printer(int idNumber, String building, int roomNumber, String type, String brand, Date dateAdded, User modifiedBy) {
+        this.idNumber = idNumber;
+        this.building = building;
+        this.roomNumber = roomNumber;
+        this.type = type;
+        this.brand = brand;
+        this.dateAdded = dateAdded;
+        this.modifiedBy = modifiedBy;
+    }
+
+    //getters and setters for all fields
+    public int getIdNumber() {
+        return idNumber;
+    }
+
+    public void setIdNumber(int idNumber) {
+        this.idNumber = idNumber;
+    }
+
+    public String getBuilding() {
+        return building;
+    }
+
+    public void setBuilding(String building) {
+        this.building = building;
+    }
+
+    public int getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(int roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public Date getDateAdded() {
+        return dateAdded;
+    }
+
+    public void setDateAdded(Date dateAdded) {
+        this.dateAdded = dateAdded;
+    }
+
+    public User getModifiedBy() {
+        return modifiedBy;
+    }
+
+    public void setModifiedBy(User modifiedBy) {
+        this.modifiedBy = modifiedBy;
+    }
+
+    @Override
+    public String toString() {
+        return "Printer{" +
+                "idNumber=" + idNumber +
+                ", building='" + building + '\'' +
+                ", roomNumber=" + roomNumber +
+                ", type='" + type + '\'' +
+                ", brand='" + brand + '\'' +
+                ", dateAdded=" + dateAdded +
+                ", modifiedBy=" + modifiedBy +
+                '}';
+    }
+}
