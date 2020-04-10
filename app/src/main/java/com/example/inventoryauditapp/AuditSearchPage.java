42c14ea86a6b29b71ac42f60bd41ac721ecf5a99
TheXenonPhoenix <majorgamerkevin@gmail.com>
2020-04-10 12:35:49 -0500

    setup auditSearchPage XML

--- app/src/main/java/com/example/inventoryauditapp/AuditSearchPage.java
+++ app/src/main/java/com/example/inventoryauditapp/AuditSearchPage.java
@@ -3,12 +3,33 @@ package com.example.inventoryauditapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Spinner;
 
 public class AuditSearchPage extends AppCompatActivity {
+
+    private Spinner itemSpinner;
+    private Spinner buildingSpinner;
+    private Spinner roomSpinner;
+
+    private Button searchButton;
+    private Button resetButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_audit_search_page);
+        
+        initUI();
+    }
+
+    //Initializes Components
+    private void initUI() {
+        itemSpinner     = findViewById(R.id.itemSpinner);
+        buildingSpinner = findViewById(R.id.buildingSpinner);
+        roomSpinner     = findViewById(R.id.roomSpinner);
+
+        searchButton    = findViewById(R.id.searchButton);
+        resetButton     = findViewById(R.id.resetButton);
     }
 }
