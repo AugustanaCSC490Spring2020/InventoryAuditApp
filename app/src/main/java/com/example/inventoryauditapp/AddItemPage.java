6fe4fc7d9d263b1d7d6ae1b7b74fad9c3aae1d6b
dalugas <trilugas@gmail.com>
2020-04-14 12:18:11 -0500

    fixed tge layout and layout functionality problems with the Add Item Page. Still not writing to the database

--- app/src/main/java/com/example/inventoryauditapp/AddItemPage.java
+++ app/src/main/java/com/example/inventoryauditapp/AddItemPage.java
@@ -6,15 +6,22 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class AddItemPage extends AppCompatActivity {
 
+    //Initialize Buttons
     Button submit;
     Button cancel;
-    EditText itemInput;
+
+    //Initialize Spinner
+    Spinner itemType;
+
+    //Initialize EditText
     EditText buildingInput;
     EditText roomInput;
     EditText osInput;
@@ -22,7 +29,11 @@ public class AddItemPage extends AppCompatActivity {
     EditText lastScannedInput;
     EditText dateAddedInput;
     EditText modifiedByInput;
-    EditText typeInput;
+
+    //Initialize TextView
+    TextView osTextView;
+    TextView lastScannedTextView;
+
 
 
 
@@ -40,13 +51,26 @@ public class AddItemPage extends AppCompatActivity {
         //Cancel Button
         changeActivity(cancel, InventorySearchResultsPage.class);
 
+        //sets a listener to see if the spinner is changed
+        itemType.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                handleItemSpinnerChange();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                //left intentionally blank
+            }
+        });
+
     }
 
     //Initializes Components
     private void initUI() {
         submit              = findViewById(R.id.submitButton);
         cancel              = findViewById(R.id.cancelButton);
-        itemInput           = findViewById(R.id.itemInput);
+        itemType            = findViewById(R.id.itemTypeSpinner);
         buildingInput       = findViewById(R.id.buildingInput);
         roomInput           = findViewById(R.id.roomInput);
         osInput             = findViewById(R.id.osInput);
@@ -54,7 +78,8 @@ public class AddItemPage extends AppCompatActivity {
         lastScannedInput    = findViewById(R.id.lastScannedInput);
         dateAddedInput      = findViewById(R.id.dateAddedInput);
         modifiedByInput     = findViewById(R.id.modifiedInput);
-        typeInput           = findViewById(R.id.typeInput);
+        osTextView          = findViewById(R.id.OSText);
+        lastScannedTextView = findViewById(R.id.lastScannedText);
     }
 
     /**
@@ -70,5 +95,27 @@ public class AddItemPage extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    /**
+     * OS and Last Scanned should only be visible when a Computer is selected
+     */
+    public void handleItemSpinnerChange(){
+        String itemTypeText = itemType.getSelectedItem().toString();
+        if(itemTypeText.equals("Computer")){
+            osTextView.setVisibility(View.VISIBLE);
+            osInput.setVisibility(View.VISIBLE);
+            lastScannedTextView.setVisibility(View.VISIBLE);
+            lastScannedInput.setVisibility(View.VISIBLE);
+        }
+        else if(itemTypeText.equals("Printer")){
+            osTextView.setVisibility(View.INVISIBLE);
+            osInput.setVisibility(View.INVISIBLE);
+            lastScannedTextView.setVisibility(View.INVISIBLE);
+            lastScannedInput.setVisibility(View.INVISIBLE);
+        }
+        else{
+            //could just be an if - else statement but leaving room for more items being added
+        }
     }
 }
