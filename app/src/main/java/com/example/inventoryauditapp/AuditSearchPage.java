3bc36164136f4b49bcf8c695c6563c93c16b2fff
TheXenonPhoenix <majorgamerkevin@gmail.com>
2020-04-15 20:17:48 -0500

    added parentActivity hierarchy to activities

--- app/src/main/java/com/example/inventoryauditapp/AuditSearchPage.java
+++ app/src/main/java/com/example/inventoryauditapp/AuditSearchPage.java
@@ -19,7 +19,7 @@ public class AuditSearchPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_audit_search_page);
-        
+
         initUI();
     }
 
