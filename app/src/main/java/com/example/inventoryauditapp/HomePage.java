045e084e736b754c07cc59fe94ac82295bc15083
dalugas <trilugas@gmail.com>
2020-05-05 12:16:20 -0500

    Added google sign in authentication and the ability to sign out.

--- app/src/main/java/com/example/inventoryauditapp/HomePage.java
+++ app/src/main/java/com/example/inventoryauditapp/HomePage.java
@@ -1,5 +1,6 @@
 package com.example.inventoryauditapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
@@ -7,12 +8,26 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class HomePage extends AppCompatActivity {
 
     private Button inventoryButton;
     private Button auditButton;
     private Button aboutButton;
+    private SignInButton signInButton;
+    private GoogleSignInClient client;
+    int RC_SIGN_IN = 0;
+    private Button signOutButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +44,25 @@ public class HomePage extends AppCompatActivity {
 
         //About Button
         changeActivity(aboutButton, AboutPage.class);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        client = GoogleSignIn.getClient(this, gso);
+
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(v == signInButton){
+                    signIn();
+                }
+            }
+        });
+
+        signOutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signOut();
+            }
+        });
     }
 
     //Initializes Components
@@ -36,6 +70,8 @@ public class HomePage extends AppCompatActivity {
         inventoryButton = findViewById(R.id.inventoryButton);
         auditButton     = findViewById(R.id.auditButton);
         aboutButton     = findViewById(R.id.aboutButton);
+        signInButton    = findViewById(R.id.signInButton);
+        signOutButton   = findViewById(R.id.signOutButton);
     }
 
     /**
@@ -52,4 +88,48 @@ public class HomePage extends AppCompatActivity {
             }
         });
     }
+
+    //Method that handles signing into a Google Account
+    private void signIn(){
+        Intent signInIntent = client.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    /**
+     *
+     * @param requestCode
+     * @param resultCode
+     * @param data
+     */
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode,resultCode,data);
+
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try{
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            Toast.makeText(this, "Sign in successfully!", Toast.LENGTH_SHORT).show();
+        }catch (ApiException e){
+            Toast.makeText(this, "Could not sign in! Status Code is " + e.getStatusCode(), Toast.LENGTH_SHORT).show();
+
+        }
+    }
+
+    private void signOut() {
+        client.signOut().addOnCompleteListener(this, new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                Toast.makeText(HomePage.this, "Signed out Successfully!", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        });
+    }
+
+
 }
