d2db056b17feec20f6c9f0d66f671d3380f0698d
dalugas <trilugas@gmail.com>
2020-05-12 09:25:31 -0500

    Added the login screen, changed homepage, and added new xml file for the new homepage design.

--- app/src/main/java/com/example/inventoryauditapp/HomePage.java
+++ app/src/main/java/com/example/inventoryauditapp/HomePage.java
@@ -11,11 +11,8 @@ import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.common.SignInButton;
-import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 
@@ -24,7 +21,6 @@ public class HomePage extends AppCompatActivity {
     private Button inventoryButton;
     private Button auditButton;
     private Button aboutButton;
-    private SignInButton signInButton;
     private GoogleSignInClient client;
     int RC_SIGN_IN = 0;
     private Button signOutButton;
@@ -32,7 +28,7 @@ public class HomePage extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_home_page);
 
         initUI();
 
@@ -68,15 +64,6 @@ public class HomePage extends AppCompatActivity {
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
         client = GoogleSignIn.getClient(this, gso);
 
-        signInButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(v == signInButton){
-                    signIn();
-                }
-            }
-        });
-
         signOutButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -90,7 +77,6 @@ public class HomePage extends AppCompatActivity {
         inventoryButton = findViewById(R.id.inventoryButton);
         auditButton     = findViewById(R.id.auditButton);
         aboutButton     = findViewById(R.id.aboutButton);
-        signInButton    = findViewById(R.id.signInButton);
         signOutButton   = findViewById(R.id.signOutButton);
     }
 
@@ -109,44 +95,13 @@ public class HomePage extends AppCompatActivity {
         });
     }
 
-    //Method that handles signing into a Google Account
-    private void signIn(){
-        Intent signInIntent = client.getSignInIntent();
-        startActivityForResult(signInIntent, RC_SIGN_IN);
-    }
-
-    /**
-     *
-     * @param requestCode
-     * @param resultCode
-     * @param data
-     */
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data){
-        super.onActivityResult(requestCode,resultCode,data);
-
-        if (requestCode == RC_SIGN_IN) {
-            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
-            handleSignInResult(task);
-        }
-    }
-
-    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
-        try{
-            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
-            Toast.makeText(this, "Sign in successfully!", Toast.LENGTH_SHORT).show();
-        }catch (ApiException e){
-            Toast.makeText(this, "Could not sign in! Status Code is " + e.getStatusCode(), Toast.LENGTH_SHORT).show();
-
-        }
-    }
-
     private void signOut() {
         client.signOut().addOnCompleteListener(this, new OnCompleteListener<Void>() {
             @Override
             public void onComplete(@NonNull Task<Void> task) {
                 Toast.makeText(HomePage.this, "Signed out Successfully!", Toast.LENGTH_SHORT).show();
-                finish();
+                //finish();
+                startActivity(new Intent(getBaseContext(), LoginStartPage.class));
             }
         });
     }
